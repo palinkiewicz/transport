79dcab1f6d4392f00ce7f99b4e54fd2a3d5bb258
palinkiewicz <pogromca.ap@gmail.com>
2026-08-08 21:57:02 +0200

    Update app's version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "pl.dakil.transport"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
